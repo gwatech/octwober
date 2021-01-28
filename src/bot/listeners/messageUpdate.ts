1f25f8025b44223eb750afd1cb2b2283bd5f6864
itsayushch <ayush.chowdhury2004@gmail.com>
2021-01-28 13:31:22 +0530

    update params

--- src/bot/listeners/messageUpdate.ts
+++ src/bot/listeners/messageUpdate.ts
@@ -5,7 +5,7 @@ import { Message, MessageReaction } from "discord.js";
 
 const functions = new Functions();
 
-const MessageUpdateListener: Listener = async (client: Client, message: Message):Promise<Message | MessageReaction> => {
+const MessageUpdateListener: Listener = async (client: Client, _: Message, message: Message):Promise<Message | MessageReaction> => {
     if (message.partial) return;
     if (message.channel.type === 'dm' || message.author.bot) return;
 

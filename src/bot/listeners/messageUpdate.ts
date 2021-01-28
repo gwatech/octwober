ef677c47872c281c3e41d00c70c545b5ab30aefc
itsayushch <ayush.chowdhury2004@gmail.com>
2021-01-28 13:27:01 +0530

    handle edits

---
+++ src/bot/listeners/messageUpdate.ts
@@ -0,0 +1,16 @@
+import Client from "../client/client";
+import { Listener } from "../core/Types";
+import Functions from '../core/funtions';
+import { Message, MessageReaction } from "discord.js";
+
+const functions = new Functions();
+
+const MessageUpdateListener: Listener = async (client: Client, message: Message):Promise<Message | MessageReaction> => {
+    if (message.partial) return;
+    if (message.channel.type === 'dm' || message.author.bot) return;
+
+    const data = await functions.parseCommand(message);
+    if (data?.command) return data.command.exec(client, message, data.content);
+}
+
+export default MessageUpdateListener;

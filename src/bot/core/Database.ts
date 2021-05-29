7df83a59fac1e50a8955c7f5fa1993601ad78791
itsayushch <ayush.chowdhury2004@gmail.com>
2021-05-30 00:48:14 +0530

    rewrite with discord-akairo

--- src/bot/core/Database.ts
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-class MongoDB extends MongoClient {
-	constructor(options: Object) {
-		super(process.env.DB, options);
-	}
-}
-
-export default MongoDB;
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import Env from 'dotenv';
-Env.config();
-
-import Client from './bot/client/client';
-const client = new Client();
-
-client.start(process.env.TOKEN);
